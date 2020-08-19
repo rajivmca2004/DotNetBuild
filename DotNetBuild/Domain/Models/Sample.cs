e884393e963f4a50fa256354e43efd530cc898d3
generic-digi-user <66323049+generic-digi-user@users.noreply.github.com>
2020-08-19 12:11:11 +0530

    My First Commit

---
+++ DotNetBuild/Domain/Models/Sample.cs
@@ -0,0 +1,12 @@
+
+namespace DotNetBuild.Domain.Models
+{
+    public class Sample
+{
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Details { get; set; }
+    }
+}
